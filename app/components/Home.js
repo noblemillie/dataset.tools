fd8e2de8a8497aedd700acc15aed7ecbe159cfca
joshuapawlik <joshawesome12@yahoo.com>
2017-12-06 14:36:30 -0600

    At 3rd step of 3 of Oauth

--- app/components/Home.js
+++ app/components/Home.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Home.css';
 import axios from 'axios';
-import { Input } from 'semantic-ui-react';
-require('dotenv').config();
+// import { Input } from 'semantic-ui-react';
+
 // import 'semantic-ui-css/semantic.min.css';
 // import ButtonExampleColored from "./button.js"
 
@@ -20,16 +20,10 @@ export default class Home extends Component {
       <div>
         <div className={styles.container} data-tid="container">
           <h2>dataset.tools</h2>
-          <input placeholder="Username"></input>
           <br/>
-          {/* <ButtonExampleColored/> */}
-          <input placeholder="Password"></input>
           <br/>
-          <button> Sign up</button>
-          <button>Login</button>
-          <button onClick={clicked}>Click Me</button>
           <script>//<Link to="/counter">to Counter</Link></script>
-          <a  href="https://data.world/oauth/authorize?client_id="${process.env.client_ID}"&redirect_uri=http://localhost:8080&response_type=code">Click here to sign in with data.world!!</a>
+          <a  href="http://localhost:8080/authorize">Click here to sign in with data.world!!</a>
         </div>
       </div>
     );
--- server/app.js
+++ server/app.js
@@ -1,6 +1,12 @@
+require('dotenv').config();
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var axios = require('axios');
+var sha256 = require('sha256');
+var base64 = require('base64url');
+// var ascii = require('ascii85');
+// code_challenge: base64(sha256(ascii.encode('pik7o7SHajQiuxahvNE297E2sF9UqJjLpPhh0ZGD25Ktg7ptHIB6A5GsyRD8CHpQ')))
 
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
@@ -8,15 +14,42 @@ app.use(bodyParser.json());
 app.listen(8080, function(){
   console.log('listening on port 8080');
 })
-
-
+// const code_verifier = "kBPZPENCUAfHyZRoGicqwhuzDawVgtpLsUpfJEvQgGbg6iEHqiteoDjrtgaErwEJ"
+const config = {
+  client_id: process.env.CLIENT_ID,
+  redirect_uri: 'http://localhost:8080',
+  response_type: 'code',
+  code_verifier: 'g0KGoCuzBjaEYmbIHdci2UGoeCmp7uBRnJrbESi5nXp1FiBayGySC4bJiqNVWiAb',
+  code_challenge: base64(sha256('g0KGoCuzBjaEYmbIHdci2UGoeCmp7uBRnJrbESi5nXp1FiBayGySC4bJiqNVWiAb'))
+}
+app.get('/authorize', function(req, res) {
+  console.log('-----------------------',config.code_challenge)
+  console.log('~~~~~AUTHORIZING')
+  const client_id = process.env.CLIENT_ID;
+  const redirect_uri = process.env.REDIRECT_URI;
+  const endpoint = process.env.AUTHORIZATION_ENDPOINT;
+  res.redirect(`https://data.world/oauth/authorize?client_id=${config.client_id}&redirect_uri=${config.redirect_uri}&response_type=code&code_challenge_method=plain&code_challenge=${config.code_challenge}`)
+});
 
 var path = require('path')
 app.get('/', function (req, res) {
+  var code = req.query.code;
+  if (code){
+    axios.post(`https://data.world/oauth/access_token?code=${code}&client_id=${config.client_id}&client_secret=${process.env.DATAWORLD_SECRET}&grant_type=authorization_code&code_verifier=${config.code_verifier}`).then((response) =>{
+      console.log('response',response)
+      if (response.data.access_token){
+        console.log('response token',response.data.access_token);
+      } else {
+        const errorMessage = response.data.message || 'unknown error';
+        console.log('errorMessage',errorMessage);
+      }
+    }).catch(err => {
+      var errorMessage = err.message || 'unknown error';
+      console.log('errorMessage',errorMessage);
+    })
+  }
   res.sendFile(path.resolve('./app/index.html'))
 });
-
-
 
 
 
