789b8cf1d8da1fc015f78fd13f3c4d273b184167
joshuapawlik <joshawesome12@yahoo.com>
2017-12-06 19:17:13 -0600

    Same as last time

--- server/app.js
+++ server/app.js
@@ -3,10 +3,10 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var axios = require('axios');
+var path = require('path');
 var sha256 = require('sha256');
 var base64 = require('base64url');
-// var ascii = require('ascii85');
-// code_challenge: base64(sha256(ascii.encode('pik7o7SHajQiuxahvNE297E2sF9UqJjLpPhh0ZGD25Ktg7ptHIB6A5GsyRD8CHpQ')))
+// var base64 = require('url-safe-base64');
 
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
@@ -14,7 +14,6 @@ app.use(bodyParser.json());
 app.listen(8080, function(){
   console.log('listening on port 8080');
 })
-// const code_verifier = "kBPZPENCUAfHyZRoGicqwhuzDawVgtpLsUpfJEvQgGbg6iEHqiteoDjrtgaErwEJ"
 const config = {
   client_id: process.env.CLIENT_ID,
   redirect_uri: 'http://localhost:8080',
@@ -28,24 +27,22 @@ app.get('/authorize', function(req, res) {
   const client_id = process.env.CLIENT_ID;
   const redirect_uri = process.env.REDIRECT_URI;
   const endpoint = process.env.AUTHORIZATION_ENDPOINT;
-  res.redirect(`https://data.world/oauth/authorize?client_id=${config.client_id}&redirect_uri=${config.redirect_uri}&response_type=code&code_challenge_method=plain&code_challenge=${config.code_challenge}`)
+  res.redirect(`https://data.world/oauth/authorize?client_id=${config.client_id}&redirect_uri=${config.redirect_uri}&response_type=code&code_challenge_method=S256&code_challenge=${config.code_challenge}`)
 });
 
-var path = require('path')
 app.get('/', function (req, res) {
   var code = req.query.code;
+  // console.log('code',code);
   if (code){
     axios.post(`https://data.world/oauth/access_token?code=${code}&client_id=${config.client_id}&client_secret=${process.env.DATAWORLD_SECRET}&grant_type=authorization_code&code_verifier=${config.code_verifier}`).then((response) =>{
       console.log('response',response)
       if (response.data.access_token){
         console.log('response token',response.data.access_token);
-      } else {
-        const errorMessage = response.data.message || 'unknown error';
-        console.log('errorMessage',errorMessage);
       }
-    }).catch(err => {
-      var errorMessage = err.message || 'unknown error';
-      console.log('errorMessage',errorMessage);
+      // else {
+      //   const errorMessage = response.data.message || 'unknown error';
+      //   console.log('errorMessage',errorMessage);
+      // }
     })
   }
   res.sendFile(path.resolve('./app/index.html'))
