5b5a7c6d07d7af35cf4617b58c4cb16eb94477c7
Ben Collins <ben@Bens-MacBook-Air.local>
2017-12-11 13:55:22 -0600

    switches between projects and datasets tags

--- app/components/Projects.js
+++ app/components/Projects.js
@@ -1,2 +1,20 @@
-export const Projects = props =>( <p>               projects-------------------!! </p>
-)
+import React, { Component } from 'react';
+export default class Projects extends Component {
+  constructor(props){
+    super(props);
+  }
+ render() {
+   return (
+     <p>Hi im in projects</p>
+   )
+ }
+}
+/*
+export const Projects = props => {
+  return (
+    <div>
+      <p>  projects-------------------!! </p>
+    </div>
+  )
+}
+*/
